246f336671b7eb25fd5641eb068905fdd0697cff
Joel Rahman <joel@flowmatters.com.au>
2024-07-18 10:43:09 +1000

    initial commit

---
+++ setup.py
@@ -0,0 +1,32 @@
+RELEASE = True
+
+import codecs
+from setuptools import setup, find_packages
+import sys, os
+
+classifiers = """\
+Development Status :: 5 - Production/Stable
+Environment :: Console
+Intended Audience :: Developers
+Intended Audience :: Science/Research
+License :: OSI Approved :: ISC License (ISCL)
+Operating System :: OS Independent
+Programming Language :: Python
+Topic :: Scientific/Engineering
+Topic :: Software Development :: Libraries :: Python Modules
+"""
+
+version = '0.1'
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+def read(*parts):
+    """
+    Build an absolute path from *parts* and and return the contents of the
+    resulting file.  Assume UTF-8 encoding.
+    """
+    with codecs.open(os.path.join(HERE, *parts), "rb", "utf-8") as f:
+        return f.read()
+
+setup(
+)
