e8c4d0b44f5755076817302bf776950478599559
Joel Rahman <joel@flowmatters.com.au>
2024-09-19 15:49:01 +1000

    Refactor separate project module

---
+++ fire_impacts/pre/project.py
@@ -0,0 +1,60 @@
+import os
+from glob import glob
+import shutil
+import logging
+logger = logging.getLogger(__name__)
+
+def initialise_project(directory:str,catchment_shapefiles=[],exist_ok=False,clear=False):
+    '''
+    Initialise a folder structure to contain working data for fire impacts studies.
+
+    Parameters:
+    - directory (str): Path to the directory where the project folder will be created.
+    - catchment_shapefiles (list): List of paths to shapefiles representing catchments.
+    - exist_ok (bool): OPTIONAL: If False, raise an error if the project folder already exists. Default is False.
+    - clear (bool): OPTIONAL: If True, clear the project folder if it already exists. Default is False.
+    '''
+    folder_paths = {}
+    # Define initial folder structure
+    subfolders = ['Output', 'Output/Topography', 'Output/FireSeverity','Output/Topography/Catchments_DEM']
+    # Create folders and store their paths
+    for folder in subfolders:
+        path = os.path.join(directory, folder)
+        if os.path.exists(path) and clear:
+            logger.info('Clearing folder: %s',path)
+            shutil.rmtree(path)
+        os.makedirs(path, exist_ok=exist_ok)
+        folder_name = folder.split('/')[-1]
+        folder_paths[folder_name] = path
+        # globals()[folder_name] = path
+
+    folder_paths['Catchment_Shapefiles'] = catchment_shapefiles
+    folder_paths['Catchment_Names'] = [os.path.splitext(os.path.basename(shapefile))[0] for shapefile in catchment_shapefiles]
+    catchment_folders = create_catchment_folders(folder_paths)
+    folder_paths.update(catchment_folders)
+    return folder_paths
+
+def create_catchment_folders(project):
+    catchment_names = project['Catchment_Names']
+    topography_path = project['Topography']
+    folder_paths = {}
+    for catch_name in catchment_names:
+        main_path = os.path.join(topography_path, catch_name)
+        os.makedirs(main_path, exist_ok=True)
+        # Create subfolders inside each main folder
+        subfolders = ['HW_SHPs', 'HW_Rasters', 'Catchment_DEM']
+        for subfolder in subfolders:
+            subfolder_path = os.path.join(main_path, subfolder)
+            os.makedirs(subfolder_path, exist_ok=True)
+        folder_name = catch_name.split('/')[-1]
+        folder_paths[folder_name] = main_path
+        # globals()[folder_name] = main_path
+    return folder_paths
+
+def find_all_shapefiles(base_directory):
+    '''
+    Find all shapefiles in a directory and its subdirectories.
+    '''
+    assert os.path.isdir(base_directory), f"Directory not found: {base_directory}"
+    shapefiles = glob(os.path.join(base_directory, '**','*.shp'),recursive=True)
+    return shapefiles
--- fire_impacts/pre/topography.py
+++ fire_impacts/pre/topography.py
@@ -1,5 +1,4 @@
 import os
-from glob import glob
 import numpy as np
 import pandas as pd
 import geopandas as gpd
@@ -13,64 +12,8 @@ from affine import Affine
 from pysheds.grid import Grid
 from scipy.ndimage import label
 import copy
-import shutil
 import logging
 logger = logging.getLogger(__name__)
-
-def initialise_project(directory:str,catchment_shapefiles=[],exist_ok=False,clear=False):
-    '''
-    Initialise a folder structure to contain working data for fire impacts studies.
-
-    Parameters:
-    - directory (str): Path to the directory where the project folder will be created.
-    - catchment_shapefiles (list): List of paths to shapefiles representing catchments.
-    - exist_ok (bool): OPTIONAL: If False, raise an error if the project folder already exists. Default is False.
-    - clear (bool): OPTIONAL: If True, clear the project folder if it already exists. Default is False.
-    '''
-    folder_paths = {}
-    # Define initial folder structure
-    subfolders = ['Output', 'Output/Topography', 'Output/Topography/Catchments_DEM']
-    # Create folders and store their paths
-    for folder in subfolders:
-        path = os.path.join(directory, folder)
-        if os.path.exists(path) and clear:
-            logger.info('Clearing folder: %s',path)
-            shutil.rmtree(path)
-        os.makedirs(path, exist_ok=exist_ok)
-        folder_name = folder.split('/')[-1]
-        folder_paths[folder_name] = path
-        # globals()[folder_name] = path
-
-    folder_paths['Catchment_Shapefiles'] = catchment_shapefiles
-    folder_paths['Catchment_Names'] = [os.path.splitext(os.path.basename(shapefile))[0] for shapefile in catchment_shapefiles]
-    catchment_folders = create_catchment_folders(folder_paths)
-    folder_paths.update(catchment_folders)
-    return folder_paths
-
-def create_catchment_folders(project):
-    catchment_names = project['Catchment_Names']
-    topography_path = project['Topography']
-    folder_paths = {}
-    for catch_name in catchment_names:
-        main_path = os.path.join(topography_path, catch_name)
-        os.makedirs(main_path, exist_ok=True)
-        # Create subfolders inside each main folder
-        subfolders = ['HW_SHPs', 'HW_Rasters', 'Catchment_DEM']
-        for subfolder in subfolders:
-            subfolder_path = os.path.join(main_path, subfolder)
-            os.makedirs(subfolder_path, exist_ok=True)
-        folder_name = catch_name.split('/')[-1]
-        folder_paths[folder_name] = main_path
-        # globals()[folder_name] = main_path
-    return folder_paths
-
-def find_all_shapefiles(base_directory):
-    '''
-    Find all shapefiles in a directory and its subdirectories.
-    '''
-    assert os.path.isdir(base_directory), f"Directory not found: {base_directory}"
-    shapefiles = glob(os.path.join(base_directory, '**','*.shp'),recursive=True)
-    return shapefiles
 
 def extract_catchment_dems(project,dem_path,target_resolution=None):
   '''
@@ -230,7 +173,7 @@ def find_closest_to_threshold(acc, row, col, threshold_cells):
                 closest_cell = (adj_row, adj_col)
 
     return closest_cell
-    
+
 def extract_headwaters(project,name,threshold_m2):
     # Extract CRS and transform and copy meta from DEM to write headwaters
     logger.info(f'Extracting headwaters for catchment: {name}')
