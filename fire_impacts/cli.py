bda8511f542305a669fca0664f682cdce4f3f87a
Joel Rahman <joel@flowmatters.com.au>
2025-11-14 09:31:08 +1100

    command line interface for initialising project

---
+++ fire_impacts/cli.py
@@ -0,0 +1,58 @@
+# my_package/cli.py
+import shutil
+import typer
+from .pre import FireImpactsProject
+import os
+import jupytext
+import logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger('fire-impacts-cli')
+
+app = typer.Typer()
+
+TEMPLATE_NOTEBOOKS=[
+    'PrepareData',
+    'Simulation'
+]
+
+@app.command()
+def new(path: str, notebooks: bool = True):
+    """Creates a new project at the specified path."""
+    logger.info(f"Creating a new project at %s", path)
+    proj = FireImpactsProject(path)
+
+    if notebooks:
+        copy_notebooks_to(path)
+
+def copy_notebooks_to(path:str):
+    logger.info("Adding template notebooks...")
+    for nb in TEMPLATE_NOTEBOOKS:
+        src = os.path.join(
+            os.path.dirname(__file__),'..', 'templates', f'{nb}.py'
+        )
+        dest = os.path.join(path, f'{nb}.py')
+        if os.path.exists(dest):
+            logger.warning('Notebook script %s already exists, skipping.', dest)
+        else:
+            shutil.copy(src, dest)
+            logger.info('Copied template: %s', dest)
+        nb_dest = os.path.join(path, f'{nb}.ipynb')
+        if os.path.exists(nb_dest):
+            logger.warning('Notebook %s already exists, skipping.', nb_dest)
+        else:
+            jupytext.write(jupytext.read(dest,fmt='py:percent'),
+                            nb_dest,
+                            fmt='ipynb')
+    logger.info("Template notebooks added.")
+    logger.info('Start with "%s.ipynb" to prepare data.', TEMPLATE_NOTEBOOKS[0])
+
+@app.command()
+def update(path:str):
+    """Updates an existing project to the latest structure."""
+    logger.info(f"Updating project at %s", path)
+    copy_notebooks_to(path)
+    logger.info("Project updated.")
+
+if __name__ == "__main__":
+    app()
+
