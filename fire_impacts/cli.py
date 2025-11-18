7fef45ba25a9d9cd217d8cbffcc382698d6de4c4
Joel Rahman <joel@flowmatters.com.au>
2025-11-18 13:45:59 +1100

    option to overwrite existing notebooks when updating project

--- fire_impacts/cli.py
+++ fire_impacts/cli.py
@@ -24,20 +24,20 @@ def new(path: str, notebooks: bool = True):
     if notebooks:
         copy_notebooks_to(path)
 
-def copy_notebooks_to(path:str):
+def copy_notebooks_to(path:str, overwrite:bool=False):
     logger.info("Adding template notebooks...")
     for nb in TEMPLATE_NOTEBOOKS:
         src = os.path.join(
             os.path.dirname(__file__),'..', 'templates', f'{nb}.py'
         )
         dest = os.path.join(path, f'{nb}.py')
-        if os.path.exists(dest):
+        if os.path.exists(dest) and not overwrite:
             logger.warning('Notebook script %s already exists, skipping.', dest)
         else:
             shutil.copy(src, dest)
             logger.info('Copied template: %s', dest)
         nb_dest = os.path.join(path, f'{nb}.ipynb')
-        if os.path.exists(nb_dest):
+        if os.path.exists(nb_dest) and not overwrite:
             logger.warning('Notebook %s already exists, skipping.', nb_dest)
         else:
             jupytext.write(jupytext.read(dest,fmt='py:percent'),
@@ -47,10 +47,10 @@ def copy_notebooks_to(path:str):
     logger.info('Start with "%s.ipynb" to prepare data.', TEMPLATE_NOTEBOOKS[0])
 
 @app.command()
-def update(path:str):
+def update(path:str,overwrite:bool=False):
     """Updates an existing project to the latest structure."""
     logger.info(f"Updating project at %s", path)
-    copy_notebooks_to(path)
+    copy_notebooks_to(path,overwrite=overwrite)
     logger.info("Project updated.")
 
 if __name__ == "__main__":
