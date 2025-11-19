e2201a6d790ad69d9cf8a4d56550c3a528065028
Joel Rahman <joel@flowmatters.com.au>
2025-11-19 11:04:30 +1100

    high level imports

--- fire_impacts/sim/__init__.py
+++ fire_impacts/sim/__init__.py
@@ -1,5 +1,5 @@
 
 from .rainfall import aggregate_rainfall_data, flatten_pyraingen_rainfall, \
                       convert_rainfall_depth_to_intensity, convert_rainfall_to_dataframe
-from .rusle import lumped_daily_rusle, gridded_total_rusle
+from .rusle import lumped_daily_rusle, gridded_total_rusle, run_usle_simulation
 from .debris import debris_flow
