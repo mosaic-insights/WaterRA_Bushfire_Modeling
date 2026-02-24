ed9131d78e81314bb02ed87ed41e8aa0d0f215bc
Richard Farr <richard.farr@alluvium.com>
2026-02-24 14:14:02 +1100

    Fix issue with TERN API datasets not connecting or downloading

--- fire_impacts/pre/soil.py
+++ fire_impacts/pre/soil.py
@@ -107,33 +107,66 @@ def extract_aridity_data(
     logger.info("Aridity extraction completed.")
 
 
-def get_stac(base_uri):
+def get_stac(base_uri, api_key=None):
     from pystac import Catalog, Collection
     from pystac.stac_io import RetryStacIO
     from urllib3.util import Retry
 
+    
     retries = Retry(total=5, backoff_factor=1)
-    stac_io = RetryStacIO()
+
+    headers = {'X-Api-Key': api_key} if api_key else None
+    stac_io = RetryStacIO(headers=headers, retry=retries)
+
     if base_uri.endswith("catalog.json"):
         return Catalog.from_file(base_uri, stac_io=stac_io)
     return Collection.from_file(base_uri, stac_io=stac_io)
 
-def find_slga_grids(base_catalog=TERN_SLGA_STAC,variables=None,depths=None,version='v2'):
+def find_slga_grids(
+    base_catalog=TERN_SLGA_STAC,
+    variables=None,
+    depths=None,
+    version='v2',
+    api_key=None
+    ):
     if variables is None:
         variables = ['SLT','CLY','SND','BDW']
     if depths is None:
         depths = SOIL_DEPTHS
 
-    catalog = get_stac(base_catalog)
+    catalog = get_stac(base_catalog, api_key=api_key)
     entries = list(catalog.get_children())
     relevant = [e for e in entries if e.id in variables]
     assets = []
     for cat in relevant:
+        # Name of the current soil variable e.g. CLY for clay
         variable = cat.id
+        # Get the items for the specified version:
         version_catalog = cat.get_child(version)
-        assets += [(variable,k,v.get_absolute_href()) \
-                   for k,v in version_catalog.get_assets().items() \
-                    if any(d for d in depths if d in k) and '_EV_' in k]
+        # Containter for assets for the current variable:
+        these_assets = []
+        for item in version_catalog.get_items():
+            # Filter so we only get relevant dataset types (EV) and
+            #depths (0-5, 5-15)
+            if (
+                any(d in item.id for d in depths) 
+                and '_EV_' in item.id
+                ):
+                data_key = item.id + '.tif'
+                this_id = item.id
+                asset_dict = item.assets
+                this_href = asset_dict[data_key]
+                yoosfle_toople = (
+                    variable,
+                    this_id,
+                    this_href.get_absolute_href()
+                    )
+                these_assets.append(yoosfle_toople)
+            else:
+                continue
+            
+            assets += these_assets
+        
     return assets
 
 @contextmanager
@@ -145,47 +178,68 @@ def gdal_api_key(key):
     old_env_var = os.environ.get(env_var)
     os.environ[env_var] = fn
     with open(fn,'w') as f:
-        f.write(f"x-api-key: {key}\n")
+        f.write(f"X-Api-Key: {key}\n")
     logger.info(f"Using GDAL API key from temporary file {fn}")
-    try:
-        yield
-    finally:
-        if old_env_var is None:
-            del os.environ[env_var]
-        else:
-            os.environ[env_var] = old_env_var
-        logger.info(f"Removing temporary GDAL API key file {fn}")
-        os.remove(fn)
+    with rasterio.Env(GDAL_HTTP_HEADER_FILE=fn):
+        try:
+            yield
+        finally:
+            if old_env_var is None:
+                del os.environ[env_var]
+            else:
+                os.environ[env_var] = old_env_var
+            logger.info(f"Removing temporary GDAL API key file {fn}")
+            os.remove(fn)
 
-def download_soil_data_stac(project:FireImpactsProject, catchment:str=None, api_key:str=None,
-                            base_stac_catalog=TERN_SLGA_STAC,version='v2'):
+def download_soil_data_stac(
+    project:FireImpactsProject,
+    catchment:str=None,
+    api_key:str=None,
+    base_stac_catalog=TERN_SLGA_STAC,version='v2'
+    ):
     """
-    Download soil-related data (Silt, Clay, Sand, Bulk Density) for each catchment
-    from STAC URLs using catchment boundary, and save the data in the appropriate folder.
+    Download soil-related data (Silt, Clay, Sand, Bulk Density) for 
+    each catchment from STAC URLs using catchment boundary, and save 
+    the data in the appropriate folder.
 
-    Intended for use with original TERN's STAC, which requires an API key.
-    Create a TERN account and obtain an API key from the TERN website (https://account.tern.org.au/)
+    Intended for use with original TERN's STAC, which requires an API 
+    key. Create a TERN account and obtain an API key from the TERN 
+    website (https://account.tern.org.au/)
 
     Parameters:
-    - project (fire_impacts.FireImpactsProject): A dictionary of project folders created for catchments.
-    - catchment (str): OPTIONAL: Name of the catchment to process. If None, process all catchments.
+    - project (fire_impacts.FireImpactsProject): A dictionary of 
+    project folders created for catchments.
+    - catchment (str): OPTIONAL: Name of the catchment to process. If 
+    None, process all catchments.
     - tern_api_key (str): API key for accessing the TERN STAC API.
     - base_stac_catalog (str): Base URL for the TERN STAC catalog.
     """
     if catchment is None:
-        project.for_each_catchment(lambda c: download_soil_data_stac(project,c, api_key,base_stac_catalog,version))
+        project.for_each_catchment(
+            lambda c: download_soil_data_stac(
+                project,c, api_key,base_stac_catalog,version
+                )
+            )
         return
 
     if api_key is None:
         logger.error("API key is required for STAC access.")
         raise ValueError("API key is required for STAC access.")
 
-    grids = find_slga_grids(base_stac_catalog, version=version)
-    logger.info(f"Processing catchment: {catchment} with {len(grids)} grids found")
+    grids = find_slga_grids(base_stac_catalog, version=version, api_key=api_key)
+    print(grids)
+    logger.info(
+        f'Processing catchment: {catchment} with {len(grids)} grids '
+        'found'
+        )
     with gdal_api_key(api_key):
         for var,fn,url in grids:
             logger.info('Downloading %s',fn)
             dest_dir = project.catchment_path(catchment,'Soils',var)
             os.makedirs(dest_dir, exist_ok=True)
-            dest_fn = os.path.join(dest_dir, fn)
-            clip_and_reproject_raster(url,project.boundary_files[catchment],dest_fn)
+            dest_fn = os.path.join(dest_dir, fn + '.tif')
+            clip_and_reproject_raster(
+                url,
+                project.boundary_files[catchment],
+                dest_fn
+                )
